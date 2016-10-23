73fbb4c75fa6eda0f6b04824b209583cff4bd3b3
liukun06 <451564319@qq.com>
2016-10-23 18:16:56 +0800

    get the encoding method

--- src/main/java/com/thu/warehouse/datacleaning/App.java
+++ src/main/java/com/thu/warehouse/datacleaning/App.java
@@ -1,13 +1,20 @@
 package com.thu.warehouse.datacleaning;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws FileNotFoundException {
+		String filename = "account.csv";
+		File file = new File(filename);
+		FileInputStream fileInputStream = new FileInputStream(file);
+		InputStreamReader reader = new InputStreamReader(fileInputStream);
+		System.out.println(reader.getEncoding());
+	}
 }
